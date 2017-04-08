49843865fc53ae05a53e6b88722704cd7001e1ff
Louis Popi <theh2o64@gmail.com>
2017-04-08 08:40:16 +0200

    bt: Add a barrier for msm8994
    Change-Id: I6c2cd100aaffd8533ae2833d4ccb752c2fa59840

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq (msm8994,$($(QCOM_HARDWARE_VARIANT))
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 include $(call all-named-subdir-makefiles,libbt-vendor)
 endif # is-vendor-board-platform
+endif
